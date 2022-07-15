8a403ace0349eee578492a9ef892a4160cabce59
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-07-16 02:20:29 +0600

    movies

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import './Header.css'
+import Navbar from 'react-bootstrap/Navbar';
+// import {NavLink} from "react-router-dom"
+
+
+const Header = ()=> {
+  return (
+    <Navbar className="navbar" expand="lg">
+      <Container>
+        <Navbar.Brand to="/">FilMoja</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link to="/">Home</Nav.Link>
+            <Nav.Link to="about">About</Nav.Link>
+            <Nav.Link to="show">Link</Nav.Link>
+            <Nav.Link to="book">Books</Nav.Link>
+          </Nav>
+          <Nav>
+            <button className="btn btn-primary">sign up</button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
